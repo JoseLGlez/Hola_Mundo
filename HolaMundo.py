5d7a3e2e7c20828e7939b01e90a486422fbd35f0
Jose Luis Gonzalez Diaz <joseluisgonzalezdiaz89@gmail.com>
2022-05-13 10:21:05 +0200

    Açadiendo archivo .txt

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,2 +1,8 @@
-print ("Hola Mundo")
-print ("Hola mundo 2")
+def string_to_int(s):
+    try:
+        string_to_number = int(eval(str(s)))
+		if type(temp) == int:
+            return temp
+    
+val = string_to_int('10')
+print(val)
